aaec4e0a5b2550ecd10e62e6c318eb815aa2f4a0
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2025-03-25 19:27:37 -0300

    Update logging configuration and enhance test listener functionality

--- src/test/java/com/webform/healing/BaseTest.java
+++ src/test/java/com/webform/healing/BaseTest.java
@@ -5,9 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
 import com.epam.healenium.SelfHealingDriver;
 
+import com.webform.healing.listeners.TestListener;
+
+@Listeners(TestListener.class)
 public class BaseTest {
 
     public WebDriver delegate;
